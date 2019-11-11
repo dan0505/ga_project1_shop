df593ec3a2a1756aff4d1d8247a2b6d37e8929b2
dan0505 <qiaoer0505@gmail.com>
2019-11-11 15:49:15 +1100

    fix cart_item issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,21 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
-    before_action :current_cart
+  before_action :current_cart
 
-    def current_cart
-        @current_cart ||= ShoppingCart.new(token: cart_token)
-    end
-    # use method in view too
-    helper_method :current_cart
+  def current_cart
+    @current_cart ||= ShoppingCart.new(token: cart_token)
+  end
+  # use method in view too
+  helper_method :current_cart
 
-    private
+  private
 
-    def cart_token
-        # return cart_token unless it's nil
-        return @cart_token unless @cart_token.nil?
+  def cart_token
+    # return cart_token unless it's nil
+    return @cart_token unless @cart_token.nil?
 
-        session[:cart_token] ||= SecureRandom.hex(8)
-        @cart_token = session[:cart_token]
-    end
+    session[:cart_token] ||= SecureRandom.hex(8)
+    @cart_token = session[:cart_token]
+  end
 end
